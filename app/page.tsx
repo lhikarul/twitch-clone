496128b657a90b805a7663443a156a327a64cee1
lhikarul <711hikaru@gmail.com>
2024-01-08 21:11:10 +0800

    feat: login

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
