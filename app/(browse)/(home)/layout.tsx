f6ba5ad4182804231b9d50a0de554e3885c2a1eb
lhikarul <711hikaru@gmail.com>
2024-02-12 16:39:31 +0800

    feat: navbar

---
+++ app/(browse)/(home)/layout.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react";
+
+import { Navbar } from "./_components/navbar";
+// import { Container } from "./_components/container";
+// import { Sidebar, SidebarSkeleton } from "./_components/sidebar";
+
+const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      {/* <div className="flex h-full pt-20">
+        <Suspense fallback={<SidebarSkeleton />}>
+          <Sidebar />
+        </Suspense>
+        <Container>{children}</Container>
+      </div> */}
+    </>
+  );
+};
+
+export default BrowseLayout;
